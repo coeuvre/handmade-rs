16f303c53003f97078821f050205b4e1a867dce2
Coeuvre Wong <coeuvre@gmail.com>
2018-11-13 00:18:01 +0800

    003 - Allocating a Backbuffer

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@
 
 extern crate winapi;
 
-type Error = &'static str;
+use std::mem::{zeroed, size_of};
 
+use winapi::ctypes::c_void;
 use winapi::shared::minwindef::{LPARAM, LPVOID, LRESULT, UINT, WPARAM};
-use winapi::shared::windef::{HMENU, HWND};
+use winapi::shared::windef::{HMENU, HWND, RECT, HBITMAP, HGDIOBJ, HDC};
 use winapi::um::libloaderapi::GetModuleHandleW;
-use winapi::um::winnt::LPCWCHAR;
-use winapi::um::winuser::*;
 use winapi::um::wingdi::*;
+use winapi::um::winnt::{LPCWCHAR, HANDLE};
+use winapi::um::winuser::*;
 
 macro_rules! wcstr {
     ($s:expr) => {{
@@ -23,7 +24,62 @@ macro_rules! wcstr {
     }};
 }
 
-unsafe extern "system" fn window_proc(
+type Error = &'static str;
+
+static mut RUNNING: bool = false;
+static mut BITMAP_MEMORY: *mut c_void = 0 as *mut c_void;
+static mut BITMAP_HANDLE: HBITMAP = 0 as HBITMAP;
+static mut BITMAP_DEVICE_CONTEXT: HDC = 0 as HDC;
+
+fn bitmap_info(width: i32, height: i32) -> BITMAPINFO {
+    let mut bitmap_info = unsafe { zeroed::<BITMAPINFO>() };
+    bitmap_info.bmiHeader.biSize = size_of::<BITMAPINFOHEADER>() as u32;
+    bitmap_info.bmiHeader.biWidth = width;
+    bitmap_info.bmiHeader.biHeight = height;
+    bitmap_info.bmiHeader.biPlanes = 1;
+    bitmap_info.bmiHeader.biBitCount = 32;
+    bitmap_info.bmiHeader.biCompression = BI_RGB;
+    bitmap_info
+}
+
+unsafe fn win32_resize_dib_section(width: i32, height: i32) {
+    if BITMAP_HANDLE != 0 as HBITMAP {
+        DeleteObject(BITMAP_HANDLE as HGDIOBJ);
+    }
+
+    if BITMAP_DEVICE_CONTEXT == 0 as HDC {
+        BITMAP_DEVICE_CONTEXT = CreateCompatibleDC(0 as HDC);
+    }
+
+    BITMAP_HANDLE = CreateDIBSection(
+        BITMAP_DEVICE_CONTEXT,
+        &bitmap_info(width, height),
+        DIB_RGB_COLORS,
+        &mut BITMAP_MEMORY,
+        0 as HANDLE,
+        0
+    );
+}
+
+unsafe fn win32_update_window(device_context: HDC, x: i32, y: i32, width: i32, height: i32) {
+    StretchDIBits(
+        device_context,
+        x,
+        y,
+        width,
+        height,
+        x,
+        y,
+        width,
+        height,
+        BITMAP_MEMORY,
+        &bitmap_info(width, height),
+        DIB_RGB_COLORS,
+        SRCCOPY,
+    );
+}
+
+unsafe extern "system" fn win32_main_window_proc(
     window: HWND,
     message: UINT,
     wparam: WPARAM,
@@ -31,31 +87,31 @@ unsafe extern "system" fn window_proc(
 ) -> LRESULT {
     match message {
         WM_SIZE => {
-            println!("WM_SIZE");
+            let mut client_rect = zeroed::<RECT>();
+            GetClientRect(window, &mut client_rect);
+            let width = client_rect.right - client_rect.left;
+            let height = client_rect.bottom - client_rect.top;
+            win32_resize_dib_section(width, height);
         }
         WM_DESTROY => {
+            RUNNING = false;
             println!("WM_DESTORY");
         }
         WM_CLOSE => {
+            RUNNING = false;
             println!("WM_CLOSE");
         }
         WM_ACTIVATEAPP => {
             println!("WM_ACTIVATEAPP");
         }
         WM_PAINT => {
-            let mut ps = std::mem::zeroed::<PAINTSTRUCT>();
-            let hdc = BeginPaint(window, &mut ps);
+            let mut ps = zeroed::<PAINTSTRUCT>();
+            let device_context = BeginPaint(window, &mut ps);
             let x = ps.rcPaint.left;
             let y = ps.rcPaint.top;
             let width = ps.rcPaint.right - ps.rcPaint.left;
             let height = ps.rcPaint.bottom - ps.rcPaint.top;
-            static mut OPERATION: u32 = WHITENESS;
-            if OPERATION == WHITENESS {
-                OPERATION = BLACKNESS;
-            } else {
-                OPERATION = WHITENESS;
-            }
-            PatBlt(hdc, x, y, width, height, OPERATION);
+            win32_update_window(device_context, x, y, width, height);
             EndPaint(window, &mut ps);
         }
         _ => return DefWindowProcW(window, message, wparam, lparam),
@@ -67,12 +123,12 @@ unsafe extern "system" fn window_proc(
 unsafe fn run() -> Result<(), Error> {
     let instance = GetModuleHandleW(0 as LPCWCHAR);
 
-    let mut window_class = std::mem::zeroed::<WNDCLASSW>();
+    let mut window_class = zeroed::<WNDCLASSW>();
 
     let class_name = wcstr!("HandmadeHeroWindowClass");
 
     window_class.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-    window_class.lpfnWndProc = Some(window_proc);
+    window_class.lpfnWndProc = Some(win32_main_window_proc);
     window_class.hInstance = instance;
     window_class.lpszClassName = class_name.as_ptr();
 
@@ -100,11 +156,12 @@ unsafe fn run() -> Result<(), Error> {
         return Err("Failed to create window");
     }
 
-    let mut message = std::mem::zeroed::<MSG>();
-    'game: loop {
+    let mut message = zeroed::<MSG>();
+    RUNNING = true;
+    while RUNNING {
         let result = GetMessageW(&mut message, 0 as HWND, 0, 0);
         if result <= 0 {
-            break 'game;
+            break;
         }
 
         TranslateMessage(&message);
