7cc0c4f4f5b201b09597c9a7610e1b6253f82c5f
Coeuvre Wong <coeuvre@gmail.com>
2018-11-15 23:03:20 +0800

    004 -Animating the Backbuffer

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@
 
 extern crate winapi;
 
-use std::mem::{zeroed, size_of};
+use std::mem::{size_of, zeroed};
 
-use winapi::ctypes::c_void;
 use winapi::shared::minwindef::{LPARAM, LPVOID, LRESULT, UINT, WPARAM};
-use winapi::shared::windef::{HMENU, HWND, RECT, HBITMAP, HGDIOBJ, HDC};
+use winapi::shared::windef::{HDC, HMENU, HWND, RECT};
 use winapi::um::libloaderapi::GetModuleHandleW;
+use winapi::um::memoryapi::*;
 use winapi::um::wingdi::*;
-use winapi::um::winnt::{LPCWCHAR, HANDLE};
+use winapi::um::winnt::{LPCWCHAR, MEM_COMMIT, MEM_RELEASE, PAGE_READWRITE};
 use winapi::um::winuser::*;
 
 macro_rules! wcstr {
@@ -27,15 +27,34 @@ macro_rules! wcstr {
 type Error = &'static str;
 
 static mut RUNNING: bool = false;
-static mut BITMAP_MEMORY: *mut c_void = 0 as *mut c_void;
-static mut BITMAP_HANDLE: HBITMAP = 0 as HBITMAP;
-static mut BITMAP_DEVICE_CONTEXT: HDC = 0 as HDC;
+static mut BITMAP_MEMORY: LPVOID = 0 as LPVOID;
+static mut BITMAP_WIDTH: i32 = 0;
+static mut BITMAP_HEIGHT: i32 = 0;
+static BYTES_PER_PIXEL: i32 = 4;
 
+unsafe fn render_weird_gradient(x_offset: i32, y_offset: i32) {
+    let width = BITMAP_WIDTH;
+    let height = BITMAP_HEIGHT;
+    let pitch = (width * BYTES_PER_PIXEL) as isize;
+    let mut row = BITMAP_MEMORY as *mut u8;
+    for y in 0..height {
+        let mut pixel = row as *mut u32;
+        for x in 0..width {
+            let b = x + x_offset;
+            let g = y + y_offset;
+            *pixel = (((g & 0xFF)<< 8) | (b & 0xFF)) as u32;
+            pixel = pixel.offset(1);
+        }
+        row = row.offset(pitch);
+    }
+}
+
+#[inline]
 fn bitmap_info(width: i32, height: i32) -> BITMAPINFO {
     let mut bitmap_info = unsafe { zeroed::<BITMAPINFO>() };
     bitmap_info.bmiHeader.biSize = size_of::<BITMAPINFOHEADER>() as u32;
     bitmap_info.bmiHeader.biWidth = width;
-    bitmap_info.bmiHeader.biHeight = height;
+    bitmap_info.bmiHeader.biHeight = -height;
     bitmap_info.bmiHeader.biPlanes = 1;
     bitmap_info.bmiHeader.biBitCount = 32;
     bitmap_info.bmiHeader.biCompression = BI_RGB;
@@ -43,37 +62,45 @@ fn bitmap_info(width: i32, height: i32) -> BITMAPINFO {
 }
 
 unsafe fn win32_resize_dib_section(width: i32, height: i32) {
-    if BITMAP_HANDLE != 0 as HBITMAP {
-        DeleteObject(BITMAP_HANDLE as HGDIOBJ);
+    if BITMAP_MEMORY != 0 as LPVOID {
+        VirtualFree(BITMAP_MEMORY, 0 as usize, MEM_RELEASE);
     }
 
-    if BITMAP_DEVICE_CONTEXT == 0 as HDC {
-        BITMAP_DEVICE_CONTEXT = CreateCompatibleDC(0 as HDC);
-    }
+    BITMAP_WIDTH = width;
+    BITMAP_HEIGHT = height;
 
-    BITMAP_HANDLE = CreateDIBSection(
-        BITMAP_DEVICE_CONTEXT,
-        &bitmap_info(width, height),
-        DIB_RGB_COLORS,
-        &mut BITMAP_MEMORY,
-        0 as HANDLE,
-        0
+    let bitmap_memory_size = width * height * BYTES_PER_PIXEL;
+    BITMAP_MEMORY = VirtualAlloc(
+        0 as LPVOID,
+        bitmap_memory_size as usize,
+        MEM_COMMIT,
+        PAGE_READWRITE,
     );
 }
 
-unsafe fn win32_update_window(device_context: HDC, x: i32, y: i32, width: i32, height: i32) {
+unsafe fn win32_update_window(
+    device_context: HDC,
+    window_rect: &RECT,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+) {
+    let window_width = window_rect.right - window_rect.left;
+    let window_height = window_rect.bottom - window_rect.top;
+
     StretchDIBits(
         device_context,
-        x,
-        y,
-        width,
-        height,
-        x,
-        y,
-        width,
-        height,
+        0,
+        0,
+        window_width,
+        window_height,
+        0,
+        0,
+        BITMAP_WIDTH,
+        BITMAP_HEIGHT,
         BITMAP_MEMORY,
-        &bitmap_info(width, height),
+        &bitmap_info(BITMAP_WIDTH, BITMAP_HEIGHT),
         DIB_RGB_COLORS,
         SRCCOPY,
     );
@@ -111,7 +138,9 @@ unsafe extern "system" fn win32_main_window_proc(
             let y = ps.rcPaint.top;
             let width = ps.rcPaint.right - ps.rcPaint.left;
             let height = ps.rcPaint.bottom - ps.rcPaint.top;
-            win32_update_window(device_context, x, y, width, height);
+            let mut client_rect = zeroed::<RECT>();
+            GetClientRect(window, &mut client_rect);
+            win32_update_window(device_context, &client_rect, x, y, width, height);
             EndPaint(window, &mut ps);
         }
         _ => return DefWindowProcW(window, message, wparam, lparam),
@@ -158,14 +187,30 @@ unsafe fn run() -> Result<(), Error> {
 
     let mut message = zeroed::<MSG>();
     RUNNING = true;
+
+    let mut x_offset = 0;
+    let mut y_offset = 0;
     while RUNNING {
-        let result = GetMessageW(&mut message, 0 as HWND, 0, 0);
-        if result <= 0 {
-            break;
+        while PeekMessageW(&mut message, 0 as HWND, 0, 0, PM_REMOVE)!= 0 {
+            if message.message == WM_QUIT {
+                RUNNING = false;
+            }
+
+            TranslateMessage(&message);
+            DispatchMessageW(&message);
         }
 
-        TranslateMessage(&message);
-        DispatchMessageW(&message);
+        render_weird_gradient(x_offset, y_offset);
+
+        let device_context = GetDC(window);
+        let mut client_rect = zeroed::<RECT>();
+        GetClientRect(window, &mut client_rect);
+        let width = client_rect.right - client_rect.left;
+        let height = client_rect.bottom - client_rect.top;
+        win32_update_window(device_context, &client_rect, 0, 0, width, height);
+        ReleaseDC(window, device_context);
+
+        x_offset += 1;
     }
 
     Ok(())
